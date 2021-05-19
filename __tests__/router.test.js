5eb38f79200c197a42a2a3daf4d6869e6636ca29
alanlwang1 <alanwangsd@gmail.com>
2021-05-19 14:37:58 -0700

    add part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,22 @@
 /**
  * @jest-environment jsdom
  */
-
+import {pushToHistory} from '../scripts/router.js';
+describe('Test pushToHistory function', () => {
+	let history; 
+	test('push settings state', () => {
+		history = pushToHistory('settings'); 
+		expect(history.length).toBe(2); 
+		expect(history.state.page).toBe('settings'); 
+	});
+	test('push entry state', () => {
+		history = pushToHistory('entry', 1); 
+		expect(history.length).toBe(3); 
+		expect(history.state.page).toBe('entry1'); 
+	});
+	test('push empty state', () => {
+		history = pushToHistory(); 
+		expect(history.length).toBe(4); 
+		expect(history.state).toStrictEqual({}); 
+	});
+});  
